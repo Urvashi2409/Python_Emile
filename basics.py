fd86ed504371fd5f448e17e42c53cd24b560c392
Urvashi2409 <urvashicode2410@gmail.com>
2024-01-19 15:22:24 +0530

    conditional statements and basics

---
+++ basics.py
@@ -0,0 +1,164 @@
+# print("Hello world")
+
+'''
+hello
+hi
+i am here
+'''
+
+"""
+hello
+hi
+i am here
+"""
+
+# name = input("Enter your name: ")
+# print(name)
+
+# Data types: 
+# int = 23, 456, 21000000000 
+# float = 23.4, 56.78
+# bool = True, False  
+
+# "False" --> 
+# type casting -- string to float 
+# marks = float(input("Enter your marks: "))
+# print(marks)
+
+# conditional statements 
+# age = 23 
+# age == 23: checking 
+
+# age = 14
+# if age > 18:
+#     print("You can vote")
+# elif age == 18:
+#     print("Apply for voter id")
+# else:
+#     print("You cannot vote")
+
+# age = int(input("Enter your age: "))
+# if age > 18:
+#     print("You can vote")
+# if age >= 18:
+#     print("Apply for voter id")
+# elif age > 17:
+#     print("Blah Blah")
+# else:
+#     print("You cannot vote")
+
+# if 3 == 4: 
+#     print("Equal")
+# elif (3 > 4):
+#     print("greater")
+# else:
+#     print("less")
+
+# x = 4
+# if x == 4:
+#     print("equal")
+# elif x >= 4:
+#     print("greater or equal")
+# elif x > 4:
+#     print("greater")
+# else:
+#     print("default")
+
+# relational operators or conditional operators or comparison operators : >, <, <=, >=, ==, !=
+# logical operators = and, or, not 
+
+# if 3 > 4 or 5 > 4:
+#     print("one")
+# elif 8 > 4 and 6 > 4:
+#     print("two")
+# else:
+#     print("none")
+
+# if not(3 < 4):
+#     print("one")
+# elif 4 > 3:
+#     print("two")
+# else:
+#     print("none")
+
+# if not(4 > 6) and not(8 > 5):
+#     print("one")
+# elif 4 > 5 or not(5 == 5):
+#     print("two")
+# else:
+#     print("none")
+
+# if 4 > 6 and not(8 > 5):
+#     print("one")
+# elif not(4 > 5) or not(5 == 5):
+#     print("two")
+# else:
+#     print("none")
+
+# if 4 > 3:
+#     print("three")
+# if 4 > 2:
+#     print("four")
+# if 5 > 4:
+#     print("five")
+# elif 4 == 4:
+#     print("six")
+# elif 4 > 3:
+#     print("seven")
+# else:
+#     print("default")
+
+# if the last if is true stop there , otherwise find elif which is true and stop there otherwise the ans is in else 
+# if 4 < 3:
+#      print("three")
+# if 4 > 2:
+#      print("four")
+# if 5 < 4:
+#      print("five")
+# elif 4 != 4:
+#      print("six")
+# elif 4 > 3:
+#      print("seven")
+# else:
+#      print("default")
+
+# x = 3 
+# y = 12
+# if x <= y and x > 0:
+#     print("three")
+# if x > y or y < 11:
+#     print("four")
+# if not(x < 4) and y < 24:
+#     print("five")
+# elif y != 4:
+#     print("six")
+# elif y >= x:
+#     print("seven")
+# else:
+#     print("default")
+
+x = 12
+y = 3
+z = 11
+if (x == 2 and x < 3):
+     print(x)
+if x != 5:
+    print("whatever")
+if x != 5 and y >= 5 and z <= 13:
+     print("its gonna happen")
+if z != 0 or x == 2:
+     print("you say")
+if (not(y < 10)):
+    print("you say")
+elif (x < 10 or x < 5):
+     print("okkkkkkk")
+elif (y < 10 or y <= 0):
+    print("pikachu")
+elif(z == 0 or y ==5):
+    print("pikaboo")
+else:
+    print("Default value")
+
+#whatever you say pikachu 
+    
+    
